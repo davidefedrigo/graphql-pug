b326da10930b952e4b11f4009c8c5392002149e0
Davide Fedrigo <me@davidefedrigo.com>
2019-06-13 23:58:22 +0200

    WIP

--- src/Controller/GraphqlController.php
+++ src/Controller/GraphqlController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\GraphQL\Type\Reservation;
+use GraphQL\GraphQL;
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Schema;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,9 +17,60 @@ class GraphqlController extends AbstractController
      */
     public function index()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/GraphqlController.php',
+        $query = "query {
+  echo(message: \"Hello World\")
+  reservations{id}
+}";
+
+        $variableValues = null;
+
+        $queryType = new ObjectType([
+            'name' => 'Query',
+            'fields' => [
+                'echo' => [
+                    'type' => Type::string(),
+                    'args' => [
+                        'message' => Type::nonNull(Type::string()),
+                    ],
+                    'resolve' => function ($root, $args) {
+                        return $root['prefix'] . $args['message'];
+                    }
+                ],
+                'reservations' => [
+                    'type' => Type::listOf(new Reservation()),
+                    'resolve' => function($a) {
+                        return [
+                            [
+                                'id' => 1
+                            ],
+                            [
+                                'id' => 2
+                            ]
+                        ];
+                    }
+                ]
+            ],
         ]);
+
+        $schema = new Schema([
+            'query' => $queryType
+        ]);
+
+        try {
+            $rootValue = ['prefix' => 'You said: '];
+            $result = GraphQL::executeQuery($schema, $query, $rootValue, null, $variableValues);
+            $output = $result->toArray();
+        } catch (\Exception $e) {
+            $output = [
+                'errors' => [
+                    [
+                        'message' => $e->getMessage()
+                    ]
+                ]
+            ];
+        }
+
+
+        return $this->json($output);
     }
 }
